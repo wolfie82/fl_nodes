36506a3a03fbb1e384b44116c9ae23ab237fecb5
Wilielmus <88447902+WilliamKarolDiCioccio@users.noreply.github.com>
2024-12-24 13:10:54 +0100

    Add ImprovedListener widget for enhanced pointer event handling

---
+++ lib/src/utils/improved_listener.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+
+class ImprovedListener extends StatefulWidget {
+  final Widget child;
+  final PointerDownEventListener? onPointerPressed;
+  final PointerMoveEventListener? onPointerMoved;
+  final PointerUpEventListener? onPointerReleased;
+  final PointerCancelEventListener? onPointerCanceled;
+  final PointerSignalEventListener? onPointerSignalReceived;
+  final VoidCallback? onDoubleClick;
+  final Duration doubleClickThreshold;
+
+  const ImprovedListener({
+    super.key,
+    required this.child,
+    this.onPointerPressed,
+    this.onPointerMoved,
+    this.onPointerReleased,
+    this.onPointerCanceled,
+    this.onPointerSignalReceived,
+    this.onDoubleClick,
+    this.doubleClickThreshold = const Duration(milliseconds: 300),
+  });
+
+  @override
+  State<ImprovedListener> createState() => _ImprovedListenerState();
+}
+
+class _ImprovedListenerState extends State<ImprovedListener> {
+  DateTime? _lastClickTime;
+
+  @override
+  Widget build(BuildContext context) {
+    return Listener(
+      onPointerDown: (PointerDownEvent event) {
+        final now = DateTime.now();
+        if (_lastClickTime != null &&
+            now.difference(_lastClickTime!) < widget.doubleClickThreshold) {
+          if (widget.onDoubleClick != null) {
+            widget.onDoubleClick!();
+          }
+          _lastClickTime = null; // Reset after double click
+        } else {
+          _lastClickTime = now;
+        }
+
+        if (widget.onPointerPressed != null) {
+          widget.onPointerPressed!(event);
+        }
+      },
+      onPointerMove: widget.onPointerMoved,
+      onPointerUp: widget.onPointerReleased,
+      onPointerCancel: widget.onPointerCanceled,
+      onPointerSignal: widget.onPointerSignalReceived,
+      child: widget.child,
+    );
+  }
+}
