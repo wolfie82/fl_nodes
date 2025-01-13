0a49072cfcfc4ef1d52a0cde22cc8955b8673b5d
Wilielmus <88447902+WilliamKarolDiCioccio@users.noreply.github.com>
2025-01-13 15:27:55 +0100

    Refactor color serialization to use normalized RGBA values

--- lib/src/core/utils/serialization.dart
+++ lib/src/core/utils/serialization.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 
 String colorToRGBAString(Color color) {
-  return '${color.r},${color.g},${color.b},${color.a}';
+  final r = color.r / 255.0;
+  final g = color.g / 255.0;
+  final b = color.b / 255.0;
+  final a = color.a / 255.0;
+  return '$r,$g,$b,$a';
 }
 
 Color colorFromRGBAString(String rgba) {
-  final values = rgba
-      .substring(5, rgba.length - 1)
-      .split(',')
-      .map((value) => int.parse(value))
-      .toList();
-  return Color.fromARGB(values[3], values[0], values[1], values[2]);
+  final values =
+      rgba.split(',').map((value) => double.parse(value.trim())).toList();
+
+  return Color.fromARGB(
+    (values[3] * 255).round(),
+    (values[0] * 255).round(),
+    (values[1] * 255).round(),
+    (values[2] * 255).round(),
+  );
 }
