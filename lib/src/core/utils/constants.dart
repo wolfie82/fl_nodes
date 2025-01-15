07616336a812b53b41d24a16f32abf857f8ba671
Wilielmus <88447902+WilliamKarolDiCioccio@users.noreply.github.com>
2025-01-15 12:09:46 +0100

    Fine tune constants.dart

--- lib/src/core/utils/constants.dart
+++ lib/src/core/utils/constants.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
 final kNodeEditorWidgetKey = GlobalKey();
-const kMaxEventHistory = 50;
-const kMaxClipboardHistory = 10;
+const kMaxEventHistory = 128;
 const kSpatialHashingCellSize = 2056.0;
