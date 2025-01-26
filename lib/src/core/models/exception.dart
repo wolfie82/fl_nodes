e53c1af3628fc124da1fd3af387f24978b0a73b4
Wilielmus <88447902+WilliamKarolDiCioccio@users.noreply.github.com>
2025-01-26 12:34:28 +0100

    Refactor RunnerException to use nodeId instead of NodeInstance and add JSON serialization methods

--- lib/src/core/models/exception.dart
+++ lib/src/core/models/exception.dart
@@ -1,11 +1,17 @@
-import 'entities.dart';
-
 class RunnerException implements Exception {
   final String message;
-  final NodeInstance node;
+  final String nodeId;
 
-  RunnerException(this.message, this.node);
+  RunnerException(this.message, this.nodeId);
 
   @override
   String toString() => 'RunnerException: $message';
+
+  Map<String, dynamic> toJson() => {
+        'message': message,
+        'nodeId': nodeId,
+      };
+
+  static RunnerException fromJson(Map<String, dynamic> json) =>
+      RunnerException(json['message'], json['nodeId']);
 }
