1001ef588da4b38af8272010dc933968cca97a69
Patrick Wolf <patricksbaker@gmail.com>
2025-07-22 18:05:28 -0700

    fix memory leak

--- lib/src/widgets/debug_info.dart
+++ lib/src/widgets/debug_info.dart
@@ -1,6 +1,6 @@
+import 'dart:async'; // Import 'dart:async' for StreamSubscription
 import 'package:fl_nodes/src/core/models/events.dart';
 import 'package:flutter/material.dart';
-
 import 'package:fl_nodes/src/core/controllers/node_editor/core.dart';
 
 class DebugInfoWidget extends StatefulWidget {
@@ -16,6 +16,9 @@ class DebugInfoWidget extends StatefulWidget {
 }
 
 class _DebugInfoWidgetState extends State<DebugInfoWidget> {
+  // 1. ADD THIS VARIABLE TO HOLD THE SUBSCRIPTION
+  StreamSubscription? _eventSubscription;
+
   double get viewportZoom => widget.controller.viewportZoom;
   Offset get viewportOffset => widget.controller.viewportOffset;
   int get selectionCount => widget.controller.selectedNodeIds.length;
@@ -24,13 +27,24 @@ class _DebugInfoWidgetState extends State<DebugInfoWidget> {
   void initState() {
     super.initState();
 
-    widget.controller.eventBus.events.listen((event) {
+    // 2. STORE THE SUBSCRIPTION WHEN YOU START LISTENING
+    _eventSubscription = widget.controller.eventBus.events.listen((event) {
       if (event is ViewportOffsetEvent ||
           event is ViewportZoomEvent ||
           event is NodeSelectionEvent) {
-        setState(() {});
+        // As a good practice, check if the widget is still mounted
+        if (mounted) {
+          setState(() {});
+        }
       }
     });
+  }
+
+  // 3. ADD THIS ENTIRE DISPOSE METHOD TO CANCEL THE SUBSCRIPTION
+  @override
+  void dispose() {
+    _eventSubscription?.cancel();
+    super.dispose();
   }
 
   @override
